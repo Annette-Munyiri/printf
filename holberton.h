9e7761f0e9d1a4b2004df9dce5632cb0dcf278a0
Annette-Munyiri <anmunyiri390@gmail.com>
2021-07-26 04:28:58 -0700

    Task 0

--- holberton.h
+++ holberton.h
@@ -5,7 +5,24 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct convert - defines a structure for symbols and functions
+ * @sym: The operator
+ * @f: function associated
+ */
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+/* Main functions */
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
 
 #endif
